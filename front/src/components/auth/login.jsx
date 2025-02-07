4829a8fb344ab9b41c18f5f99962a9832a45c003
darwinllanos <jhondarwinllanosnarvaez@gmail.com>
2025-02-06 21:08:40 -0500

    Estructuras Carpetas, Login, Register Estilos

--- front/src/components/auth/login.jsx
+++ front/src/components/auth/login.jsx
@@ -1,18 +1,23 @@
+import styles from '../../styles/Login.module.css';
+
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className={styles.container}>
+      <section className={styles.section}>
+        <img src="/logo.png" alt="Logo" />
+      </section>
+      <section className={styles.section}>
       <form>
+      <h2>Iniciar Sesion</h2>
         <label>
-          Nombre de usuario:
-          <input type="text" name="nameUser" />
+          <input className={styles.input} type="text" name="nameUser" placeholder='Nombre Usuario'/>
         </label>
         <label>
-          Contraseña:
-          <input type="password" name="password" />
+          <input className={styles.input} type="password" name="password" placeholder='Contraseña'/>
         </label>
-        <button type="submit">Login</button>
+        <button className={styles.button} type="submit">Ingresar</button>
       </form>
+      </section>
     </div>
   );
 }
